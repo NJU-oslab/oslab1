9e919e47cccb45dd4ea92f3025194e885c0c407f
tracer <tracer@tracer.org>
2018-05-11 23:17:25 +0800

    > build broccoli Linux ubuntu 4.13.0-37-generic #42~16.04.1-Ubuntu SMP Wed Mar 7 16:03:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- include/debug.h
+++ include/debug.h
@@ -12,8 +12,10 @@
 
 // #include <trace.h>
 #ifdef TRACEME
-  #define TRACE_ENTRY printf("[trace] %s:entry\n", __func__)
-  #define TRACE_EXIT printf("[trace] %s:exit\n", __func__)
+  #define TRACE_ENTRY \
+    printf("[trace] %s:entry\n", __func__)
+  #define TRACE_EXIT \
+    printf("[trace] %s:exit\n", __func__)
 #else
   #define TRACE_ENTRY ((void)0)
   #define TRACE_EXIT ((void)0)
