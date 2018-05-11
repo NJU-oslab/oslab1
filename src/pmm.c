88d4e99340aba0eb61b238c689576dbb98470ecd
tracer <tracer@tracer.org>
2018-05-11 23:09:03 +0800

    > build broccoli Linux ubuntu 4.13.0-37-generic #42~16.04.1-Ubuntu SMP Wed Mar 7 16:03:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- src/pmm.c
+++ src/pmm.c
@@ -8,13 +8,13 @@
 static void pmm_init();
 static void *pmm_alloc(size_t size);
 static void pmm_free(void *ptr);
-
+*/
 MOD_DEF(pmm) {
-    .init = pmm_init,
-    .alloc = pmm_alloc,
-    .free = pmm_free
+ //   .init = pmm_init,
+ //   .alloc = pmm_alloc,
+ //   .free = pmm_free
 };
-
+/*
 
 typedef union block {
 	struct {
