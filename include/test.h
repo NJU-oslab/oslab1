90a0790f2c29adebab8212b49dae84cf48451232
tracer <tracer@tracer.org>
2018-05-16 23:39:33 +0800

    > build broccoli Linux ubuntu 4.13.0-41-generic #46~16.04.1-Ubuntu SMP Thu May 3 10:06:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

---
+++ include/test.h
@@ -0,0 +1,3 @@
+void alloc_test();
+void thread_test();
+void sem_test();
--- src/os.c
+++ src/os.c
@@ -3,6 +3,8 @@
 static void os_init();
 static void os_run();
 static _RegSet *os_interrupt(_Event ev, _RegSet *regs);
+static thread_t *current_thread;
+
 
 MOD_DEF(os) {
   .init = os_init,
@@ -14,44 +16,28 @@ static void os_init() {
   for (const char *p = "Hello, OS World!\n"; *p; p++) {
     _putc(*p);
   }
-}
-
-static void alloc_test() {
-  int *arr = (int*)pmm->alloc(20 * sizeof(int));
-  for (int i = 0; i < 20; i++){
-    arr[i] = i;
-  }
-  for (int i = 0; i < 20; i++){
-    Log("arr[%d]: %d", i, arr[i]);
-  }
-  pmm->free(arr);
-  pmm->alloc(8192);
-  pmm->alloc(400);
-  pmm->alloc(1023);
-  Log("_heap.end: 0x%x", _heap.end);
-}
-
-static void thread_test() {
-
-}
-
-static void sem_test() {
-  
+  current_thread = NULL;
 }
 
 static void os_run() {
-  _intr_write(1); // enable interrupt
   alloc_test();
   thread_test();
   sem_test();
-//  while (1) ; // should never return
+  _intr_write(1);
+  while (1) ; // should never return
 }
 
 static _RegSet *os_interrupt(_Event ev, _RegSet *regs) {
-  if (ev.event == _EVENT_IRQ_TIMER) _putc('*');
-  if (ev.event == _EVENT_IRQ_IODEV) _putc('I');
+  if (ev.event == _EVENT_IRQ_TIMER){
+ //   Log("_EVENT_IRQ_TIMER");
+    if (current_thread != NULL)
+      current_thread->tf = regs;
+    current_thread = kmt->schedule();
+    return current_thread->tf;
+  } 
+  if (ev.event == _EVENT_IRQ_IODEV) Log("_EVENT_IRQ_IODEV");
   if (ev.event == _EVENT_ERROR) {
-    _putc('x');
+    Log("_EVENT_IRQ_ERROR");
     _halt(1);
   }
   return NULL; // this is allowed by AM
