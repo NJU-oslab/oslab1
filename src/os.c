8987ba3bc8ce2ceb09dae11be060f658e9ca3c45
tracer <tracer@tracer.org>
2018-05-16 23:50:46 +0800

    > build broccoli Linux ubuntu 4.13.0-41-generic #46~16.04.1-Ubuntu SMP Thu May 3 10:06:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- src/os.c
+++ src/os.c
@@ -19,6 +19,65 @@ static void os_init() {
   current_thread = NULL;
 }
 
+void alloc_test() {
+  Log("============================\nalloc_test begin...");
+  int *arr = (int*)pmm->alloc(20 * sizeof(int));
+  for (int i = 0; i < 20; i++){
+    arr[i] = i;
+  }
+  for (int i = 0; i < 20; i++){
+    Log("arr[%d]: %d", i, arr[i]);
+  }
+  pmm->free(arr);
+  pmm->alloc(8192);
+  pmm->alloc(400);
+  pmm->alloc(1023);
+  Log("_heap.end: 0x%x", _heap.end);
+  Log("alloc_test end.\n============================");
+}
+
+static thread_t test_thread[5];
+static int finished_thread[5];
+
+static void thread_test_func(void *tid){
+    int cnt;
+    for (cnt = 0; cnt < 10; cnt++){
+        Log("The %d thread prints: %d", (int)tid, cnt);
+    }
+    Log("The %d thread is finished.");
+
+}
+
+void thread_test() {
+  Log("============================\nthread_test begin...");   
+  int i;
+  for (i = 0; i < 5; i++){
+    finished_thread[i] = 0;
+    kmt->create(&test_thread[i], thread_test_func, (void *)i);
+  }
+  while (1){
+    int j;
+    int cnt = 0;
+    for (j = 0; j < 5; j++){
+        if (finished_thread[j] == 1)
+            cnt++;
+    }
+    if (cnt == 5)
+        break;
+  }
+  Log("All the thread has finished.");
+  for (i = 0; i < 5; i++)
+    kmt->teardown(&test_thread[i]);
+  Log("All the thread has been teardowned.");
+  Log("thread_test end\n============================");
+}
+
+void sem_test() {
+  Log("============================\nsem_test begin");
+
+  Log("sem_test end\n============================");
+}
+
 static void os_run() {
   alloc_test();
   thread_test();
