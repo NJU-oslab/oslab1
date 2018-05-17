9ea328e655cbfd3cc268b8c4a80e50b3e1cf3adf
tracer <tracer@tracer.org>
2018-05-17 18:33:24 +0800

    > run broccoli Linux ubuntu 4.13.0-41-generic #46~16.04.1-Ubuntu SMP Thu May 3 10:06:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- src/kmt.c
+++ src/kmt.c
@@ -186,10 +186,11 @@ static void kmt_sem_wait(sem_t *sem){
         current_thread->waiting_sem = sem;
         Log("%s: 0x%x",current_thread->waiting_sem->name, current_thread->waiting_sem);
         kmt_spin_unlock(&sem_lock);
-        while (current_thread->runnable == 0){
+//        while (current_thread->runnable == 0){
 /*			kmt_spin_lock(&sem_lock);
 			kmt_spin_unlock(&sem_lock);*/
 		}
+		_yield();
         kmt_spin_lock(&sem_lock);
     }
     sem->count --;
