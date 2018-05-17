290820ad4f446f418c9a472c03ed99be1aa912c0
tracer <tracer@tracer.org>
2018-05-17 20:05:10 +0800

    > run broccoli Linux ubuntu 4.13.0-41-generic #46~16.04.1-Ubuntu SMP Thu May 3 10:06:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- src/pmm.c
+++ src/pmm.c
@@ -6,6 +6,102 @@ static void pmm_init();
 static void *pmm_alloc(size_t size);
 static void pmm_free(void *ptr);
 
+static void *current;
+
+struct _Block {
+	size_t size; // Aligned size
+	void *start; // the start of block area
+	int free; // free = this block is occupied? 0 : 1;
+	void *next; // the next block
+}; 
+
+#define BLOCK_SIZE 16
+
+struct _Block *block;
+
+MOD_DEF(pmm) {
+	.init = pmm_init,
+	.alloc = pmm_alloc,
+	.free = pmm_free,
+};
+
+static void pmm_init() {
+	current = _heap.start;
+	block = NULL;
+}
+
+int digit(size_t size) {
+		int d = 0;
+		while (size != 0)
+		{
+			size = size >> 1;
+			d += 1;
+		}
+		return d;
+	};
+
+	size_t align(int digit) {
+		return 1 << (digit - 1);
+	};
+
+static void *pmm_alloc(size_t size) {
+
+	if (size == 0)
+	{
+		return NULL;
+	}
+	//Log("New block to be created with size %d", size);
+	if (block == NULL)
+	{
+		void *p = current;
+		while (((size_t)p) % align(digit(size + BLOCK_SIZE)) != 0) p++;
+		current = p + align(digit(size + BLOCK_SIZE));
+		//Log("current = %8x and p = %8x", (unsigned)current, (unsigned)p);
+		block = (struct _Block *)p + size;
+		block->size = align(digit(size + BLOCK_SIZE)) - BLOCK_SIZE;
+		block->start = p;
+		block->free = 0;
+		block->next = NULL;
+		return p;
+	}
+	else
+	{
+		struct _Block *p = block;
+		while (p->next != NULL)
+		{
+			if (p->size >= size && p->free)
+				return p->start;
+			p = p->next;
+		}
+		if (p->size >= size && p->free)
+			return p->start;
+		void *q = current;
+		while (((size_t)q) % align(digit(size + BLOCK_SIZE)) != 0) q++;
+		current = q + align(digit(size + BLOCK_SIZE));
+		block = (struct _Block *)p + size;
+		p->size = align(digit(size + BLOCK_SIZE)) - BLOCK_SIZE;
+		p->start = q;
+		p->free = 0;
+		p->next = NULL;
+		return p->start;
+	}
+}
+
+static void pmm_free(void *ptr) {
+	struct _Block *p = block;
+	while (p->next != NULL)
+	{
+		if (p->start == ptr)
+		{
+			p->free = 1;
+			break;
+		}
+		p = p->next;
+	}
+	if (p->start == ptr)
+		p->free = 1;
+}
+/*
 static spinlock_t pmm_lock;
 static void *cur = NULL;
 static size_t mul = 1;
@@ -99,10 +195,10 @@ static void* malloc_unsafe(size_t size) {
 	current = prev->body.next;
 	while (1) {
 		if (current->body.size >= size) {
-			if (current->body.size > size) {/*
-				current->body.size -= size;
-				current = (Block *)((char *)current + current->body.size);
-				current->body.size = size;*/
+			if (current->body.size > size) {
+		//		current->body.size -= size;
+		//		current = (Block *)((char *)current + current->body.size);
+		//		current->body.size = size;
 				size = current->body.size;
 				prev->body.next = current->body.next;
 				freep = prev;
@@ -148,3 +244,4 @@ static void pmm_free(void *ptr){
 	kmt->spin_unlock(&pmm_lock);
 	TRACE_EXIT;
 }
+*/
