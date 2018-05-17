1301d6d93c15ef68405db1fd4941c0297c18799c
tracer <tracer@tracer.org>
2018-05-17 15:17:11 +0800

    > run broccoli Linux ubuntu 4.13.0-41-generic #46~16.04.1-Ubuntu SMP Thu May 3 10:06:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- src/os.c
+++ src/os.c
@@ -64,10 +64,8 @@ void thread_test() {
     kmt->create(&test_thread[i], thread_test_func, (void *)i);
     Log("Thread %d created.", i);
   }
-  Log("All the thread has finished.");
-  for (i = 0; i < 5; i++)
-    kmt->teardown(&test_thread[i]);
-  Log("All the thread has been teardowned.");
+  kmt->teardown(&test_thread[2]);
+  Log("Thread 2 has been teardowned.");*/
   Log("thread_test end\n============================");
 }
 
