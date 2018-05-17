43f684e436642a8af0bce3b9e7893c00995526c3
tracer <tracer@tracer.org>
2018-05-17 19:52:05 +0800

    > run broccoli Linux ubuntu 4.13.0-41-generic #46~16.04.1-Ubuntu SMP Thu May 3 10:06:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- src/pmm.c
+++ src/pmm.c
@@ -100,10 +100,12 @@ static void* malloc_unsafe(size_t size) {
 	current = prev->body.next;
 	while (1) {
 		if (current->body.size >= size) {
-			if (current->body.size > size) {
+			if (current->body.size > size) {/*
 				current->body.size -= size;
 				current = (Block *)((char *)current + current->body.size);
-				current->body.size = size;
+				current->body.size = size;*/
+				size = current->body.size;
+				prev->body.next = current->body.next;
 				freep = prev;
 			}
 			else {
