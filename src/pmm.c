0daaa1b4db74262fab6da28e3c7e297eea44f493
tracer <tracer@tracer.org>
2018-05-17 20:33:08 +0800

    > run broccoli Linux ubuntu 4.13.0-41-generic #46~16.04.1-Ubuntu SMP Thu May 3 10:06:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- src/pmm.c
+++ src/pmm.c
@@ -6,7 +6,7 @@ static void pmm_init();
 static void *pmm_alloc(size_t size);
 static void pmm_free(void *ptr);
 
-
+/*
 static void *current;
 
 struct _Block {
@@ -107,9 +107,8 @@ static void pmm_free(void *ptr) {
 	}
 	if (p->start == ptr)
 		p->free = 1;
-}
+}*/
 
-/*
 
 static spinlock_t pmm_lock;
 static void *cur = NULL;
@@ -252,4 +251,4 @@ static void pmm_free(void *ptr){
 	free_unsafe(ptr);
 	kmt->spin_unlock(&pmm_lock);
 	TRACE_EXIT;
-}*/
+}
