c77a8e460113336fd1f92db3614b59bc77eab6f0
tracer <tracer@tracer.org>
2018-05-17 19:37:18 +0800

    > run broccoli Linux ubuntu 4.13.0-41-generic #46~16.04.1-Ubuntu SMP Thu May 3 10:06:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- src/pmm.c
+++ src/pmm.c
@@ -69,7 +69,7 @@ static Block* allocate_new(size_t size) {
 	TRACE_ENTRY;
 	void *new_mem;
 	Block *new_block;
-	size = align(size);
+//	size = align(size);
 	if (size + cur > _heap.end)
 		return NULL;
 	else {
