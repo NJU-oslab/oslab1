e8502bbc5d9246fbab494c559ce4196f2db8c877
tracer <tracer@tracer.org>
2018-06-07 15:25:39 +0800

    > build broccoli Linux ubuntu 4.13.0-41-generic #46~16.04.1-Ubuntu SMP Thu May 3 10:06:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- src/vfs.c
+++ src/vfs.c
@@ -27,33 +27,33 @@ static void vfs_init(){
 }
 
 static int vfs_access(const char *path, int mode){
-
+    return 0;
 }
 
 static int vfs_mount(const char *path, filesystem_t *fs){
-
+    return 0;
 }
 
 static int vfs_unmount(const char *path){
-
+    return 0;
 }
 
 static int vfs_open(const char *path, int flags){
-
+    return 0;
 }
 
 static ssize_t vfs_read(int fd, void *buf, size_t nbyte){
-
+    return 0;
 }
 
 static ssize_t vfs_write(int fd, void *buf, size_t nbyte){
-
+    return 0;
 }
 
 static off_t vfs_lseek(int fd, off_t offset, int whence){
-
+    return 0;
 }
 
 static int vfs_close(int fd){
-
+    return 0;
 }
