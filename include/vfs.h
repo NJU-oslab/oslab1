a90b3fe814404a36654ba48dd494729d92ecb053
tracer <tracer@tracer.org>
2018-06-07 19:48:56 +0800

    > build broccoli Linux ubuntu 4.13.0-43-generic #48~16.04.1-Ubuntu SMP Thu May 17 12:56:46 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- include/vfs.h
+++ include/vfs.h
@@ -3,34 +3,39 @@
 #define MAX_INODE_NAME_LEN 128
 #define MAX_INODE_CONTENT_LEN 1024
 
+typedef struct fsops fsops_t;
+typedef struct fileops fileops_t;
+typedef struct file file_t;
+typedef struct inode inode_t;
+typedef struct filesystem filesystem_t;
+
 enum open_flags{
   O_RDONLY, O_WRONLY, O_RDWR
 };
 
-typedef struct fsops {
+struct fsops {
     void (*init)(struct filesystem *fs, const char *name, inode_t *dev);
     inode_t *(*lookup)(struct filesystem *fs, const char *path, int flags);
     int (*close)(inode_t *inode);
-} fsops_t;
+};
 
-typedef struct fileops {
+struct fileops {
     int (*open)(inode_t *inode, file_t *file, int flags);
     ssize_t (*read)(inode_t *inode, file_t *file, char *buf, size_t size);
     ssize_t (*write)(inode_t *inode, file_t *file, const char *buf, size_t size);
     off_t (*lseek)(inode_t *inode, file_t *file, off_t offset, int whence);
-} fileops_t;
+};
 
-
-typedef struct file{
+struct file{
     fileops_t *ops;
-} file_t;
+};
 
-typedef struct inode{
+struct inode{
     char name[MAX_INODE_NAME_LEN];
     char content[MAX_INODE_CONTENT_LEN];
-} inode_t;
+};
 
-typedef struct filesystem{
+struct filesystem{
     char name[MAX_FS_NAME_LEN];
     fsops_t *ops;
-} filesystem_t;
+};
